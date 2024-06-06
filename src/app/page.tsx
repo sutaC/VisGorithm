7b30ccb38eb18047d8b8eebe63bf8e8ed7272b81
sutaC <catus.pl@gmail.com>
2024-06-06 20:08:03 +0200

    Ptrs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
-import ArrayDisplay from "@/components/ArrayDisplay";
+import ArrayDisplay, { Pointer } from "@/components/ArrayDisplay";
 
 export default function Home() {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+    const [ptrs, setPtrs] = useState<Pointer[]>([{ index: 0, color: "red" }]);
+
+    const updatePtr = () => {
+        const [ptr] = ptrs;
+        ptr.index++;
+        if (ptr.index >= arr.length) ptr.index = 0;
+        setPtrs([ptr]);
+    };
+
     return (
         <main className={styles.main}>
             <h1>Array</h1>
-            <ArrayDisplay array={arr}></ArrayDisplay>
+            <ArrayDisplay array={arr} pointers={ptrs}></ArrayDisplay>
+            <button onClick={updatePtr}>+</button>
         </main>
     );
 }
--- src/components/ArrayDisplay.tsx
+++ src/components/ArrayDisplay.tsx
@@ -1,10 +1,28 @@
 import style from "./ArrayDisplay.module.css";
 
-export default function ArrayDisplay(prop: { array: number[] }) {
+export interface Pointer {
+    index: number;
+    color: string;
+}
+
+export default function ArrayDisplay(prop: {
+    array: number[];
+    pointers?: Pointer[];
+}) {
     return (
         <div className={style.display}>
-            {prop.array.map((val) => (
-                <div className={style.cell}>{val}</div>
+            {prop.array.map((val, index) => (
+                <div
+                    key={index}
+                    className={style.cell}
+                    style={{
+                        background:
+                            prop.pointers?.find((ptr) => ptr.index === index)
+                                ?.color || "transparent",
+                    }}
+                >
+                    {val}
+                </div>
             ))}
         </div>
     );
