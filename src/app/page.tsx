5d9c5525c1c7ec189c29e4624fa4c111700448e8
sutaC <catus.pl@gmail.com>
2024-06-06 20:12:39 +0200

    Arr rand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,17 @@ import { useState } from "react";
 import styles from "./page.module.css";
 import ArrayDisplay, { Pointer } from "@/components/ArrayDisplay";
 
-export default function Home() {
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+function getRandomArray(size: number): number[] {
+    const array = [];
+    for (let i = 0; i < size; i++) {
+        const val = Math.floor(Math.random() * 100);
+        array.push(val);
+    }
+    return array;
+}
 
+export default function Home() {
+    const [arr, setArr] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
     const [ptrs, setPtrs] = useState<Pointer[]>([{ index: 0, color: "red" }]);
 
     const updatePtr = () => {
@@ -16,11 +24,16 @@ export default function Home() {
         setPtrs([ptr]);
     };
 
+    const randArr = () => {
+        setArr(getRandomArray(10));
+    };
+
     return (
         <main className={styles.main}>
             <h1>Array</h1>
             <ArrayDisplay array={arr} pointers={ptrs}></ArrayDisplay>
             <button onClick={updatePtr}>+</button>
+            <button onClick={randArr}>Rand</button>
         </main>
     );
 }
