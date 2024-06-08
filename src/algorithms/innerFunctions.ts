8b25d6dab94107e722a514d4badf7b8fd4f4ae99
sutaC <catus.pl@gmail.com>
2024-06-08 16:17:25 +0200

    Controls enhanced

--- src/algorithms/innerFunctions.ts
+++ src/algorithms/innerFunctions.ts
@@ -7,3 +7,12 @@ export function swap(array: number[], a: number, b: number) {
 export async function wait(time: number): Promise<void> {
     return new Promise<void>((res) => setTimeout(res, time));
 }
+
+export function getRandomArray(size: number): number[] {
+    const array = [];
+    for (let i = 0; i < size; i++) {
+        const val = Math.floor(Math.random() * 100);
+        array.push(val);
+    }
+    return array;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./page.module.css";
 import ArrayDisplay, { Pointer } from "@/components/ArrayDisplay";
-import { bubleSort, bubleSortEnhanced } from "@/algorithms/bubbleSort";
-
-function getRandomArray(size: number): number[] {
-    const array = [];
-    for (let i = 0; i < size; i++) {
-        const val = Math.floor(Math.random() * 100);
-        array.push(val);
-    }
-    return array;
-}
+import { bubleSortEnhanced } from "@/algorithms/bubbleSort";
+import { getRandomArray } from "@/algorithms/innerFunctions";
 
 export default function Home() {
     const [arr, setArr] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     const [ptrs, setPtrs] = useState<Pointer[]>([]);
 
+    let isRunningRef = useRef(false);
+    let isSortedRef = useRef(false);
+
     const randArr = () => {
+        if (isRunningRef.current) return;
         setArr(getRandomArray(10));
+        setPtrs([]);
+        isSortedRef.current = false;
     };
 
-    const sortArr = () => {
-        setArr(bubleSort(arr));
-        bubleSortEnhanced(arr, (array, pointers) => {
+    const sortArr = async () => {
+        if (isRunningRef.current) return;
+        isRunningRef.current = true;
+        await bubleSortEnhanced(arr, (array, pointers) => {
             setPtrs(pointers);
             setArr(array);
         });
+        isRunningRef.current = false;
+        isSortedRef.current = true;
     };
 
     return (
         <main className={styles.main}>
             <h1>Array</h1>
             <ArrayDisplay array={arr} pointers={ptrs}></ArrayDisplay>
-            <div>
-                <button onClick={randArr}>Rand</button>
-                <button onClick={sortArr}>Sort</button>
+            <div className={styles.controls}>
+                <button onClick={randArr} disabled={isRunningRef.current}>
+                    Rand
+                </button>
+                <button
+                    onClick={sortArr}
+                    disabled={isRunningRef.current || isSortedRef.current}
+                >
+                    Sort
+                </button>
             </div>
         </main>
     );
