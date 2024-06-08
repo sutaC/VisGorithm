96e5e3b46b00074f9dad6c301666663dfb22a5c8
sutaC <catus.pl@gmail.com>
2024-06-08 16:09:18 +0200

    Sorted state

--- src/algorithms/bubbleSort.ts
+++ src/algorithms/bubbleSort.ts
@@ -28,10 +28,13 @@ export async function bubleSortEnhanced(
     update: (array: number[], pointers: Pointer[]) => void
 ): Promise<void> {
     const arr = structuredClone(array);
+
+    const sortedPtrs: Pointer[] = [];
+
     for (let i = 0; i < arr.length; i++) {
         for (let j = 1; j < arr.length - i; j++) {
             // Traverse
-            update(arr, [pointer(j - 1, styles.index)]);
+            update(arr, [pointer(j - 1, styles.index), ...sortedPtrs]);
             await wait(500);
 
             if (arr[j - 1] > arr[j]) {
@@ -39,6 +42,7 @@ export async function bubleSortEnhanced(
                 update(arr, [
                     pointer(j - 1, styles.swapGreater),
                     pointer(j, styles.swapSmaller),
+                    ...sortedPtrs,
                 ]);
                 await wait(1000);
 
@@ -49,16 +53,21 @@ export async function bubleSortEnhanced(
                 update(arr, [
                     pointer(j - 1, styles.nonSwap),
                     pointer(j, styles.nonSwap),
+                    ...sortedPtrs,
                 ]);
                 await wait(500);
             }
 
             // Next
-            update(arr, [pointer(j - 1, styles.index)]);
+            update(arr, [pointer(j - 1, styles.index), ...sortedPtrs]);
             await wait(500);
         }
+        // Sorted section
+        sortedPtrs.push(pointer(arr.length - i - 1, styles.sorted));
     }
 
     // End
-    update(arr, []);
+    sortedPtrs.push(pointer(0, styles.sorted));
+    sortedPtrs.push(pointer(1, styles.sorted));
+    update(arr, sortedPtrs);
 }
