0cb51031e8ec321fd4b77c873fcf7a48ad7370a6
sutaC <catus.pl@gmail.com>
2024-06-16 22:47:08 +0200

    Queue animations

--- src/components/ds/QueueDisplayEnhanced.tsx
+++ src/components/ds/QueueDisplayEnhanced.tsx
@@ -1,39 +1,75 @@
 "use client";
 
+import queueStyles from "@/components/QueueDisplay.module.css";
 import styles from "./QueueDisplayEnhanced.module.css";
 import { useState } from "react";
 import { QueueEnhanced } from "@/algorithms/ds/Queue";
 import { QueueDisplay } from "../QueueDisplay";
-import { getRandomArray } from "@/algorithms/innerFunctions";
+import {
+    Pointer,
+    getRandomArray,
+    pointer,
+    wait,
+} from "@/algorithms/innerFunctions";
 
 export function QueueDisplayEnhanced() {
     const [queue, setQueue] = useState(new QueueEnhanced([0, 0, 0, 0, 0]));
+    const [pointers, setPointers] = useState<Pointer[]>([]);
+    const [processing, setProcessing] = useState(false);
 
-    const handleEnqueue = () => {
+    const update = () => setQueue(new QueueEnhanced(queue.getStructure()));
+
+    const handleEnqueue = async () => {
+        if (processing) return;
         if (queue.length >= 5) return;
+        setProcessing(true);
+
         const [val] = getRandomArray(1);
+
         queue.enqueue(val);
-        setQueue(new QueueEnhanced(queue.getStructure()));
+        setPointers([pointer(queue.length - 1, queueStyles.enqueue)]);
+        update();
+
+        await wait(500);
+        setPointers([]);
+
+        setProcessing(false);
     };
 
-    const handleDequeue = () => {
+    const handleDequeue = async () => {
+        if (processing) return;
         if (queue.length === 0) return;
+
+        setProcessing(true);
+
+        setPointers([pointer(0, queueStyles.dequeue)]);
+        await wait(500);
+
         queue.dequeue();
-        setQueue(new QueueEnhanced(queue.getStructure()));
+        setPointers([]);
+        update();
+
+        setProcessing(false);
     };
 
     return (
         <div className={styles.display}>
             <div className={styles.controlls}>
-                <button disabled={queue.length >= 5} onClick={handleEnqueue}>
+                <button
+                    disabled={queue.length >= 5 || processing}
+                    onClick={handleEnqueue}
+                >
                     Enqueue
                 </button>
-                <button disabled={queue.length === 0} onClick={handleDequeue}>
+                <button
+                    disabled={queue.length === 0 || processing}
+                    onClick={handleDequeue}
+                >
                     Dequeue
                 </button>
             </div>
 
-            <QueueDisplay queue={queue}></QueueDisplay>
+            <QueueDisplay queue={queue} pointers={pointers}></QueueDisplay>
         </div>
     );
 }
