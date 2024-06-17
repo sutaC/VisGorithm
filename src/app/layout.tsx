bd220b8f5fff87df0b68a867376b6c1253de4eda
sutaC <catus.pl@gmail.com>
2024-06-17 14:08:42 +0200

    Site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "VisGorithm",
+    description: "Algorithm visualizer",
+    keywords: "algorithm visualistaion",
+    creator: "sutaC",
+};
 
 export default function RootLayout({
     children,
